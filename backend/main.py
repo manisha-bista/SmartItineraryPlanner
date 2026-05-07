de5a969f099b88b47fa31712a87987a52550a2bf
Sujit Mainali <200~manishabista033@gmail.com>
2026-05-07 03:08:41 -0500

    removed env variable in cors origins

--- backend/main.py
+++ backend/main.py
@@ -46,7 +46,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[os.getenv("FRONTEND_URL", "https://smart-itinerary-planner.vercel.app"), "http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["https://smart-itinerary-planner.vercel.app", "http://localhost:5173", "http://127.0.0.1:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
