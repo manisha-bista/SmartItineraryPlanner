f737d21b150a387c61e821cdb48f5a9784451fe0
Sujit Mainali <200~manishabista033@gmail.com>
2026-05-07 02:16:48 -0500

    main file changed for backend deployment

--- backend/main.py
+++ backend/main.py
@@ -93,6 +93,6 @@ def health_check():
         db.close()
 
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
